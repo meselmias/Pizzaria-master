2c03012f9b9207d63681fe4b1e89431a2d80377d
meselmias <meselmias16@gmail.com>
2015-02-25 22:33:38 -0300

    ds

---
+++ Sistema Pizzaria UNIP/Projeto Pim/Projeto Pim/Projeto Pim/Controllers/ClienteController.cs
@@ -0,0 +1,78 @@
+﻿using System.Web.Mvc;
+using Projeto_Pim.Models;
+using Projeto_Pim.Models.BLogic;
+
+namespace Projeto_Pim.Controllers
+{
+    public class ClienteController : Controller
+    {
+        //lista clientes
+        [HttpGet]
+        public ActionResult Lista()
+        {
+            return View(ClienteBL.ListaClientes());
+        }
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Mensagem = "Ops!Ocorreu um erro ao Cadastrar!";
+                return View();
+            }
+            var retorno = ClienteBL.Cadastrar(cliente);
+            return RedirectToAction("Lista");
+        }
+
+        [HttpGet]
+        public ActionResult Detalhe(int id = 0)
+        {
+            var cliente = ClienteBL.ResgatarDadosClientePorId(id);
+
+            return View(cliente);
+        }
+
+        [HttpGet]
+        public ActionResult Excluir(int id = 0)
+        {
+            var retorno = ClienteBL.ExcluiClientePorId(id);
+            return Json(new
+            {
+                sucess = retorno,
+                redirectUrl = Url.Action("Lista", "Cliente"),
+
+            });
+
+
+        }
+
+        [HttpPost]
+        public ActionResult Excluir(Cliente cliente)
+        {
+            TempData["cliente"] = cliente;
+            return RedirectToAction("Detalhe", cliente);
+        }
+
+        public ActionResult Alterar(int id = 0)
+        {
+            var cliente = ClienteBL.ResgatarDadosClientePorId(id);
+
+            return View(cliente);
+        }
+
+        [HttpPost]
+        public ActionResult Alterar(Cliente cliente)
+        {
+            var retorno = ClienteBL.AtualizarCliente(cliente);
+            ViewBag.Mensagem = retorno ? "Alterado com sucesso !!!" : "OPS! ocorreu algum erro com a alteração";
+            return View(cliente);
+        }
+    }
+}
---
+++ Sistema Pizzaria UNIP/Projeto Pim/Projeto Pim/Projeto Pim/Controllers/PedidoController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projeto_Pim.Models;
+using Projeto_Pim.Models.BLogic;
+
+namespace Projeto_Pim.Controllers
+{
+    public class PedidoController : Controller
+    {
+
+        public ActionResult Lista()
+        {
+            return View(PedidoBL.ListaPedidos());
+        }
+
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            ComboClientes();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Pedido pedido)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Mensagem = "Ops!Ocorreu um erro ao Cadastrar!";
+                ComboClientes();
+                return View();
+            }
+            var retorno = PedidoBL.Cadastrar(pedido);
+            ComboClientes();
+            return RedirectToAction("Detalhes", new { id = retorno });
+        }
+
+        [HttpGet]
+        public ActionResult Detalhes(int id = 0)
+        {
+            var retorno = PedidoBL.ResgatarPedidoPorId(id);
+
+            return View(retorno);
+        }
+
+        [HttpGet]
+        public ActionResult Excluir(int id = 0)
+        {
+            var retorno = PedidoBL.ExcluiPedidoPorId(id);
+
+            return Json(new
+            {
+                sucess = retorno,
+                redirectUrl = Url.Action("Lista", "Pedido"),
+
+            });
+        }
+
+        public ActionResult Alterar(int id = 0)
+        {
+            var pedido = PedidoBL.ResgatarPedidoPorId(id);
+
+            return View(pedido);
+        }
+
+        [HttpPost]
+        public ActionResult Alterar(Pedido pedido)
+        {
+            var retorno = PedidoBL.Atualizar(pedido);
+            ViewBag.Mensagem = retorno ? "Alterado com sucesso" : "OPS! ocorreu algum erro com a alteração";
+            return View(pedido);
+        }
+
+        public ActionResult _Modal()
+        {
+            return PartialView();
+        }
+
+
+        private void ComboClientes()
+        {
+            var listaClientes = ClienteBL.ListaClientes();
+            listaClientes.Insert(0, new Cliente {Nome = "Selecione um cliente", Id = 0});
+            ViewBag.ListaClientes = listaClientes;
+        }
+
+    }
+}
